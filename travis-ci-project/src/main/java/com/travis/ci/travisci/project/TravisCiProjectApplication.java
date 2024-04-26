877f92263bd424bdc43108c081f277038ca0e454
suryareddy2151 <166613193+suryareddy2151@users.noreply.github.com>
2024-04-26 11:56:47 +0530

    Add files via upload

---
+++ travis-ci-project/src/main/java/com/travis/ci/travisci/project/TravisCiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.travis.ci.travisci.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisCiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisCiProjectApplication.class, args);
+	}
+
+}
---
+++ travis-ci-project/src/test/java/com/travis/ci/travisci/project/TravisCiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travis.ci.travisci.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravisCiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
